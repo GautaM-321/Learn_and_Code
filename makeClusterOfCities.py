28a3a5be3352b349c3e5e80e91599c3deefc9f14
Gautam Sharma <gautam.s@intimetec.com>
2023-10-19 09:04:13 +0530

    merge conflcits resolved

--- makeClusterOfCities.py
+++ makeClusterOfCities.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 # According to the same longitude considering all latitude coordinates at the same line of 90 degrees
 
@@ -39,44 +38,3 @@ def displayClusters():
         
 readLocationCsvfile()
 displayClusters()        
-=======
-import csv
-# According to same longitude considering all latitude coordinates at same line of 90degree
-
-ClusterRange=[20,40,60,80,90]
-clusters=[{},{},{},{},{}]
-
-
-def putLocationsIntoCluster(latitudeValueOfLocation,location):
-    
-    ClusterId=0
-    for eachRangeOfCluster in ClusterRange:
-        ClusterId=ClusterId+1
-        if latitudeValueOfLocation<=eachRangeOfCluster:
-        
-            clusters[ClusterId-1]=location
-            break
-
-
-def convertDegreeStringToNumber(latitude_String,location):
-    value_Of_Latitude_Degree = ""
-    for eachCharacter in latitude_String:
-       if eachCharacter == "°":
-        break  
-       value_Of_Latitude_Degree += eachCharacter
-       putLocationsIntoCluster(int(value_Of_Latitude_Degree),location)
-    
-with open('locations.csv', 'r') as file:
- 
-    locations = csv.DictReader(file)
-    
-    for location in locations:
-        
-        convertDegreeStringToNumber(location["Latitude"],location)
-           
-    
-       
-           
-# readLocationCsvfile()        
-print(clusters)
->>>>>>> d8e245b5be795488bf8ceaf7d9acbb38e68ff6ef
