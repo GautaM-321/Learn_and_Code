feecc2b506d57113cb486201e6c366d40bf0875d
Gautam Sharma <gautam.s@intimetec.com>
2023-11-30 11:45:49 +0530

    refactored code

--- Kapconstant/Kc.js
+++ Kapconstant/Kc.js
@@ -1,19 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var readlineSync = require("readline-sync");
+function getAscendingOrder(digits) {
+    var ascending = parseInt(digits.sort().join(""), 10);
+    return ascending;
+}
+function getDecendingOrder(digits) {
+    var descending = parseInt(digits.sort(function (a, b) { return b - a; }).join(""), 10);
+    return descending;
+}
+function convertNumberToSingleDigitArray(number) {
+    var numberArray = number.toString().split("").map(Number);
+    return numberArray;
+}
 function kaprekarRoutine(number) {
     var numberOfIteration = 0;
     while (number !== 6174) {
-        var digits = number.toString().split('').map(Number);
-        var ascending = parseInt(digits.sort().join(''), 10);
-        var descending = parseInt(digits.sort(function (a, b) { return b - a; }).join(''), 10);
+        var digitArray = convertNumberToSingleDigitArray(number);
+        var ascending = getAscendingOrder(digitArray);
+        var descending = getDecendingOrder(digitArray);
         number = descending - ascending;
         console.log("".concat(descending, " - ").concat(ascending, " = ").concat(number));
         numberOfIteration = numberOfIteration + 1;
     }
     console.log("kaprekar's contant is reached and number of iteration is -> ".concat(numberOfIteration));
 }
-// Example: Starting with a random four-digit number
-// const initialNumber: number = 3524;
-var userInput = readlineSync.question('Enter a number: ');
-kaprekarRoutine(Number(userInput));
+function isValidFourDigitNumber(enteredNumber) {
+    if (1000 <= enteredNumber && enteredNumber < 10000) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function convertStringToNumber(enteredString) {
+    var number = Number(enteredString);
+    return number;
+}
+function takeInputFromUser() {
+    var userInput = readlineSync.question("Enter a 4 digit number: ");
+    var enteredNumber = convertStringToNumber(userInput);
+    if (isValidFourDigitNumber(enteredNumber)) {
+        kaprekarRoutine(Number(userInput));
+    }
+    else {
+        console.log("User entered invalid number");
+    }
+}
+takeInputFromUser();
--- Kapconstant/Kc.ts
+++ Kapconstant/Kc.ts
@@ -1,21 +1,58 @@
-import * as readlineSync from 'readline-sync';
-function kaprekarRoutine(number: number): void {
-    let numberOfIteration=0;
-    while (number !== 6174) {
-        const digits: number[] = number.toString().split('').map(Number);
-        const ascending: number = parseInt(digits.sort().join(''), 10);
-        const descending: number = parseInt(digits.sort((a, b) => b - a).join(''), 10);
-        number = descending - ascending;
-        console.log(`${descending} - ${ascending} = ${number}`);
-        numberOfIteration=numberOfIteration+1;
-    }
-
-    console.log(`kaprekar's contant is reached and number of iteration is -> ${numberOfIteration}`);
+import * as readlineSync from "readline-sync";
+function getAscendingOrder(digits: number[]) {
+  const ascending: number = parseInt(digits.sort().join(""), 10);
+  return ascending;
 }
 
-function takeInputFromUser(){
-    const userInput: string = readlineSync.question('Enter a 4 digit number: ');
+function getDecendingOrder(digits: number[]) {
+  const descending: number = parseInt(
+    digits.sort((a, b) => b - a).join(""),
+    10
+  );
+  return descending;
+}
+
+function convertNumberToSingleDigitArray(number: Number) {
+  const numberArray: number[] = number.toString().split("").map(Number);
+  return numberArray;
+}
+function kaprekarRoutine(number: number): void {
+  let numberOfIteration = 0;
+  while (number !== 6174) {
+    const digitArray: number[] = convertNumberToSingleDigitArray(number);
+    const ascending = getAscendingOrder(digitArray);
+    const descending = getDecendingOrder(digitArray);
+    number = descending - ascending;
+    console.log(`${descending} - ${ascending} = ${number}`);
+    numberOfIteration = numberOfIteration + 1;
+  }
+
+  console.log(
+    `kaprekar's contant is reached and number of iteration is -> ${numberOfIteration}`
+  );
+}
+
+function isValidFourDigitNumber(enteredNumber: number) {
+  if (1000 <= enteredNumber && enteredNumber < 10000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function convertStringToNumber(enteredString: string) {
+  const number = Number(enteredString);
+  return number;
+}
+
+function takeInputFromUser() {
+  const userInput: string = readlineSync.question("Enter a 4 digit number: ");
+  const enteredNumber = convertStringToNumber(userInput);
+  if (isValidFourDigitNumber(enteredNumber)) {
     kaprekarRoutine(Number(userInput));
+  } else {
+    console.log("User entered invalid number");
+  }
 }
 
 takeInputFromUser();
